2e677795e9de2dd90ec9b8df8540d542affa7e68
wahbean <wahbean@users.noreply.github.com>
2015-07-08 03:07:19 -0400

    new file

---
+++ app/src/main/java/com/example/wmck/spotifystreamer/AlertHandler.java
@@ -0,0 +1,61 @@
+package com.example.wmck.spotifystreamer;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+/**
+ * Created by wmck on 07/07/15.
+ *
+ * Helper class used to handle alert messages to user.
+ *
+ */
+public class AlertHandler {
+
+    public static void showAlertDialog(String title, String message, Context context)
+    {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        final AlertDialog alertDialog = dialogBuilder.create();
+
+
+        TextView titleView = new TextView(context);
+        titleView.setText(title);
+        titleView.setGravity(Gravity.CENTER_HORIZONTAL);
+        titleView.setPadding(0, 0, 0, 5);
+
+        TextView messageView = new TextView(context);
+        messageView.setWidth(FrameLayout.LayoutParams.MATCH_PARENT);
+        messageView.setGravity(Gravity.CENTER_HORIZONTAL);
+        messageView.setText(message);
+
+        Button dialogButton = new Button(context);
+        dialogButton.setText("Ok");
+        // need to find a way of placing button in the centre of dialog, for part 2
+        dialogButton.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+
+        dialogBuilder.setCustomTitle(titleView);
+        dialogBuilder.setView(messageView);
+        dialogBuilder.setNeutralButton(context.getString(R.string.ok_label), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialogBuilder.show();
+    }
+
+
+}
