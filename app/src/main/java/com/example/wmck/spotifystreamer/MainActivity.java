d7a46de428f2cc46156d820860b968f2e9fc100e
wahbean <wahbean@users.noreply.github.com>
2015-07-09 08:33:33 -0400

    class now extends AppCompatActivity instead of deprecated ActionBarActivity

--- app/src/main/java/com/example/wmck/spotifystreamer/MainActivity.java
+++ app/src/main/java/com/example/wmck/spotifystreamer/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.wmck.spotifystreamer;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
--- app/src/main/java/com/example/wmck/spotifystreamer/TopTrackActivity.java
+++ app/src/main/java/com/example/wmck/spotifystreamer/TopTrackActivity.java
@@ -1,12 +1,12 @@
 package com.example.wmck.spotifystreamer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TopTrackActivity extends ActionBarActivity {
+public class TopTrackActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
