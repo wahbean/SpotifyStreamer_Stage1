2e595d468d23496b5dbfaba68c06e5153048ae3e
wahbean <wahbean@users.noreply.github.com>
2015-11-27 23:09:13 -0500

    added callback method onArtistSelected

--- app/src/main/java/com/example/wmck/spotifystreamer/MainActivity.java
+++ app/src/main/java/com/example/wmck/spotifystreamer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.wmck.spotifystreamer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,10 +10,11 @@ import android.view.MenuItem;
 
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainActivityFragment.ArtistSelectedCallback {
 
     private static final String ARTIST_TOP_TRACKS_TAG = "TTTAG";
     private boolean mTwoPane;
+    int mItemPosition;
 
 
     @Override
@@ -24,14 +27,6 @@ public class MainActivity extends AppCompatActivity {
             // in two-pane mode.
             mTwoPane = true;
 
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            if (savedInstanceState == null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.artist_top_tracks, new TopTrackActivityFragment(), ARTIST_TOP_TRACKS_TAG)
-                        .commit();
-            }
         }else {
             mTwoPane = false;
         }
@@ -43,6 +38,37 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onArtistSelected(String artistId, String artistName) {
+
+        if (mTwoPane) {
+
+            ArtistInfo artistInfo = new ArtistInfo();
+            artistInfo.setArtistId(artistId);
+            artistInfo.setArtistName(artistName);
+            Bundle args = new Bundle();
+            args.putParcelable(TopTrackActivityFragment.TOPTRACKS_ARTIST_INFO, artistInfo);
+
+            // Make new fragment to show this selection.
+            TopTrackActivityFragment artistLayoutFragment = new TopTrackActivityFragment();
+            artistLayoutFragment.setArguments(args);
+
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.artist_top_tracks, artistLayoutFragment);
+            ft.commit();
+
+        } else { // one single pane
+
+            Intent intent = new Intent(this, TopTrackActivity.class);
+            intent.putExtra(getString(R.string.artist_key), artistId);
+            intent.putExtra(getString(R.string.artist_name), artistName);
+            startActivity(intent);
+        }
     }
 
     @Override
