897ee11dbd2af7b473ffc9073415a9e8ce412740
wahbean <wahbean@users.noreply.github.com>
2015-12-07 01:25:56 -0500

    made class Parcelable

--- app/src/main/java/com/example/wmck/spotifystreamer/TrackListItem.java
+++ app/src/main/java/com/example/wmck/spotifystreamer/TrackListItem.java
@@ -1,9 +1,12 @@
 package com.example.wmck.spotifystreamer;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by wmck on 02/07/15.
  */
-public class TrackListItem {
+public class TrackListItem implements Parcelable {
     String trackTitle;
     String albumName;
     String thumbnailSmallUri;
@@ -69,4 +72,38 @@ public class TrackListItem {
     public void setPreviewUrl(String previewUrl) {
         this.previewUrl = previewUrl;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.trackTitle);
+        dest.writeString(this.albumName);
+        dest.writeString(this.thumbnailSmallUri);
+        dest.writeString(this.thumbnailLargeUri);
+        dest.writeString(this.previewUrl);
+        dest.writeLong(this.trackDuration);
+    }
+
+    protected TrackListItem(Parcel in) {
+        this.trackTitle = in.readString();
+        this.albumName = in.readString();
+        this.thumbnailSmallUri = in.readString();
+        this.thumbnailLargeUri = in.readString();
+        this.previewUrl = in.readString();
+        this.trackDuration = in.readLong();
+    }
+
+    public static final Parcelable.Creator<TrackListItem> CREATOR = new Parcelable.Creator<TrackListItem>() {
+        public TrackListItem createFromParcel(Parcel source) {
+            return new TrackListItem(source);
+        }
+
+        public TrackListItem[] newArray(int size) {
+            return new TrackListItem[size];
+        }
+    };
 }
